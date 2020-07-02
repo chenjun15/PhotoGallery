e30d9fa5c46a12d44b20fc31835260151c0cb627
chenjun <437456560@qq.com>
2020-07-02 22:05:43 +0800

    第29章 broadcast intent

---
+++ app/src/main/java/com/cj/photogallery/NotificationReceiver.java
@@ -0,0 +1,27 @@
+package com.cj.photogallery;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    private static final String TAG = "NotificationReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "onReceive: received result: " + getResultCode());
+        if (getResultCode() != Activity.RESULT_OK)
+            return; // A foreground activity cancelled the broadcast
+
+        int requestCode = intent.getIntExtra(PollService.REQUEST_CODE, 0);
+        Notification notification = intent.getParcelableExtra(PollService.NOTIFICATION);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(requestCode, notification);
+    }
+}
