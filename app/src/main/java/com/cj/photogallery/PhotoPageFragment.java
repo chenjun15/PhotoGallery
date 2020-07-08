73ddb6cdfaca02da16a464574053b45a5587f8b0
chenjun <437456560@qq.com>
2020-07-08 08:21:00 +0800

    第30章 网页浏览

---
+++ app/src/main/java/com/cj/photogallery/PhotoPageFragment.java
@@ -0,0 +1,73 @@
+package com.cj.photogallery;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PhotoPageFragment extends VisibleFragment {
+    private static final String ARG_URI = "photo_page_url";
+
+    private Uri mUri;
+    private WebView mWebView;
+    private ProgressBar mProgressBar;
+
+    public static PhotoPageFragment newInstance(Uri uri) {
+        Bundle args = new Bundle();
+        args.putParcelable(ARG_URI, uri);
+
+        PhotoPageFragment fragment = new PhotoPageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mUri = getArguments().getParcelable(ARG_URI);
+    }
+
+//    @SuppressLint("SetJavaScriptEnabled")
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
+
+        mWebView = v.findViewById(R.id.web_view);
+
+        mProgressBar = v.findViewById(R.id.progress_bar);
+        mProgressBar.setMax(100); // WebChromeClient reports in range 0-100
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
+        mWebView.loadUrl(mUri.toString());
+
+        return v;
+    }
+}
